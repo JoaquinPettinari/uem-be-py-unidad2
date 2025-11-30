b7e1111d3414026c04d6d802b8a8126bd0f9f340
joaquin.pettinari <joaquin.pettinari@hotmail.com>
2025-11-30 20:25:57 +0100

    Create service for spotify router

--- app/routers/spotify.py
+++ app/routers/spotify.py
@@ -1,34 +1,16 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.services.spotify_service import get_spotify_token, SPOTIFY_SEARCH_URL
-from app.models.search_history_model import SearchHistory, SearchTypeEnum
-from app.models.music_action_model import MusicAction, ActionEnum
+from app.models.search_history_model import SearchTypeEnum
+from app.models.music_action_model import ActionEnum
 from app.schemas.user import User
+from app.services.spotify_service import get_search, get_music_action
 from app.utils.dependencies import get_db, get_user_or_404
-import requests
 
 router = APIRouter(prefix="/spotify", tags=["Spotify"])
 
 @router.get("/search")
 def search_spotify(query: str, user: User = Depends(get_user_or_404),  type: SearchTypeEnum = SearchTypeEnum.track, db: Session = Depends(get_db)):
-    token = get_spotify_token()
-
-    headers = {"Authorization": f"Bearer {token}"}
-    params = {"q": query, "type": type, "limit": 10}
-
-    response = requests.get(SPOTIFY_SEARCH_URL, headers=headers, params=params)
-    if response.status_code != 200:
-        raise HTTPException(500, "Spotify API error")
-
-    db_search = SearchHistory(
-        user_id=user.id,
-        query=query,
-        type=type,
-    )
-    db.add(db_search)
-    db.commit()
-
-    return response.json()
+    return get_search(query, user, type, db)
 
 @router.post("/action")
 def music_action(
@@ -38,20 +20,4 @@ def music_action(
     type: SearchTypeEnum = SearchTypeEnum.track,
     db: Session = Depends(get_db)
 ):
-    if isinstance(action, ActionEnum):
-        raise HTTPException(400, "Action must be 'like' or 'dislike'")
-    if isinstance(type, SearchTypeEnum):
-        raise HTTPException(400, "Type must be 'track', 'album' or 'artist'")
-
-    action = MusicAction(
-        user_id=user.id,
-        spotify_id=spotify_id,
-        type=type,
-        action=action
-    )
-
-    db.add(action)
-    db.commit()
-    db.refresh(action)
-
-    return {"message": f"{action} saved", "data": action}
+    return get_music_action(user, action, spotify_id, type, db)
--- app/services/spotify_service.py
+++ app/services/spotify_service.py
@@ -1,20 +1,56 @@
 import base64
 import os
 import requests
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
 from dotenv import load_dotenv
+from app.models.search_history_model import SearchHistory, SearchTypeEnum
+from app.schemas.user import User
+from app.models.music_action_model import MusicAction, ActionEnum
 
 load_dotenv()
 
 SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
 SPOTIFY_SEARCH_URL = "https://api.spotify.com/v1/search"
 
+def get_search(query: str, user: User, type: SearchTypeEnum, db: Session):
+    spotify_search = get_spotify_search(query, type)
+    db_search = SearchHistory(
+        user_id=user.id,
+        query=query,
+        type=type,
+    )
+    db.add(db_search)
+    db.commit()
+    db.refresh(db_search)
+
+    return spotify_search
+
+def get_music_action(user: User, action: ActionEnum, spotify_id: str, type: SearchTypeEnum, db: Session ):
+    if not isinstance(action, ActionEnum):
+        raise HTTPException(400, "Action must be 'like' or 'dislike'")
+    if not isinstance(type, SearchTypeEnum):
+        raise HTTPException(400, "Type must be 'track', 'album' or 'artist'")
+
+    action = MusicAction(
+        user_id=user.id,
+        spotify_id=spotify_id,
+        type=type,
+        action=action
+    )
+
+    db.add(action)
+    db.commit()
+    db.refresh(action)
+
+    return {"message": f"{action} saved", "data": action}
 
 def get_spotify_token():
     client_id = os.getenv("SPOTIFY_CLIENT_ID")
     client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
     if not client_id or not client_secret:
-        raise Exception("Spotify credentials are missing in environment variables")
+        raise HTTPException(500, "Spotify credentials are missing in environment variables")
 
     auth_string = f"{client_id}:{client_secret}"
     auth_base64 = base64.b64encode(auth_string.encode()).decode()
@@ -30,3 +66,16 @@ def get_spotify_token():
 
     token = response.json().get("access_token")
     return token
+
+def get_spotify_search(query: str, type: SearchTypeEnum):
+    token = get_spotify_token()
+
+    headers = {"Authorization": f"Bearer {token}"}
+    params = {"q": query, "type": type, "limit": 10}
+
+    request = requests.get(SPOTIFY_SEARCH_URL, headers=headers, params=params)
+
+    if request.status_code != 200:
+        raise HTTPException(500, "Spotify API error")
+
+    return request.json()
